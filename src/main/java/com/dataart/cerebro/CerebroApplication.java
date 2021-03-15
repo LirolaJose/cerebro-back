d6ba84afc651cee4a588dd4523dffdd48e902a30
klirola <khose.lirola@dataart.com>
2021-03-15 11:37:08 +0300

    changed the order of writing the date

--- src/main/java/com/dataart/cerebro/CerebroApplication.java
+++ src/main/java/com/dataart/cerebro/CerebroApplication.java
@@ -1,13 +1,17 @@
 package com.dataart.cerebro;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class })
+@Slf4j
 public class CerebroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CerebroApplication.class, args);
+		log.info("Application is run");
 	}
 
 }
