9cd6be22737dd72879a792dc881dcc0328ef32f0
klirola <khose.lirola@dataart.com>
2021-03-15 10:56:17 +0300

    create project, add data base script

---
+++ src/main/java/com/dataart/cerebro/CerebroApplication.java
@@ -0,0 +1,13 @@
+package com.dataart.cerebro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerebroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerebroApplication.class, args);
+	}
+
+}
