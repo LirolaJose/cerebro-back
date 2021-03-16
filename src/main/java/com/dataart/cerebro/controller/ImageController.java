475a3cb97a0b98699f399581a3785ed48f6e6e34
klirola <khose.lirola@dataart.com>
2021-03-16 17:29:29 +0300

    customized the showing image by id

--- src/main/java/com/dataart/cerebro/controller/ImageController.java
+++ src/main/java/com/dataart/cerebro/controller/ImageController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -23,20 +24,20 @@ public class ImageController {
     }
 
 
-    @RequestMapping(value = "/sid", method = RequestMethod.GET,
-            produces = MediaType.IMAGE_JPEG_VALUE)
-
-    public void getImage(HttpServletResponse response) throws IOException {
-        var imgFile = new ClassPathResource("image/sid.jpg");
-        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
-    }
+//    @RequestMapping(value = "/sid", method = RequestMethod.GET,
+//            produces = MediaType.IMAGE_JPEG_VALUE)
+//
+//    public void getImage(HttpServletResponse response) throws IOException {
+//        var imgFile = new ClassPathResource("image/sid.jpg");
+//        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+//        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+//    }
 
     @RequestMapping(value = "/image", method = RequestMethod.GET,
             produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage(){
+    public ResponseEntity<byte[]> getImage(@RequestParam int id){
 
-        byte[] image = advertisementDAO.getImageByAdvertisementId(1);
+        byte[] image = advertisementDAO.getImageByAdvertisementId(id);
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.IMAGE_JPEG)
