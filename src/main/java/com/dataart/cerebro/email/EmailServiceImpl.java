127184451954493426c819169116075fcb85dab3
klirola <khose.lirola@dataart.com>
2021-04-06 14:36:23 +0300

    Added thread pool to EmailServiceImpl

--- src/main/java/com/dataart/cerebro/email/EmailServiceImpl.java
+++ src/main/java/com/dataart/cerebro/email/EmailServiceImpl.java
@@ -10,22 +10,25 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class EmailServiceImpl implements EmailService {
     private final JavaMailSender emailSender;
+    private final Executor executor = Executors.newFixedThreadPool(5);
 
     @Value("${spring.data.rest.page-param-name}")
     private String url;
 
     private static final String SIGNATURE = "CEREBRO: \n +7 (222) 555-77-99 \n lirolaboard@gmail.com";
-
 
     public EmailServiceImpl(JavaMailSender emailSender) {
         this.emailSender = emailSender;
@@ -33,119 +36,135 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendEmailAboutPublication(AdvertisementDTO advertisement, ContactInfoDTO contactInfo) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        String email = contactInfo.getEmail();
-        StringBuilder text = new StringBuilder();
+        executor.execute(() -> {
+            log.info("Sending email about publication is started at {}", LocalDateTime.now());
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            String email = contactInfo.getEmail();
+            StringBuilder text = new StringBuilder();
 
-        mailMessage.setTo(email);
-        mailMessage.setSubject("Your advertisement is added");
-        text.append(advertisement.getTitle()).append("\n")
-                .append(advertisement.getText()).append("\n \n")
-                .append(SIGNATURE);
-        mailMessage.setText(text.toString());
-        emailSender.send(mailMessage);
-        log.info("Email about publication is sent to {}", email);
+            mailMessage.setTo(email);
+            mailMessage.setSubject("Your advertisement is added");
+            text.append(advertisement.getTitle()).append("\n")
+                    .append(advertisement.getText()).append("\n \n")
+                    .append(SIGNATURE);
+            mailMessage.setText(text.toString());
+            emailSender.send(mailMessage);
+            log.info("Email about publication is sent to {} at {}", email, LocalDateTime.now());
+        });
     }
 
     @Override
     public void sendEmailAboutExpiring(Map<String, List<AdvertisementDTO>> emailAndAds) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        executor.execute(() -> {
+            log.info("Sending email about expiring is started at {}", LocalDateTime.now());
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
 
-        emailAndAds.forEach(((email, advertisementsList) -> {
-            ContactInfoDTO contactInfo = advertisementsList.get(0).getOwner();
-            mailMessage.setTo(contactInfo.getEmail());
-            mailMessage.setSubject("Advertisement will be closed soon");
+            emailAndAds.forEach(((email, advertisementsList) -> {
+                ContactInfoDTO contactInfo = advertisementsList.get(0).getOwner();
+                mailMessage.setTo(contactInfo.getEmail());
+                mailMessage.setSubject("Advertisement will be closed soon");
 
-            StringBuilder text = new StringBuilder();
-            text.append("Dear ").append(contactInfo.getName()).append(",\n").append("your advertisement(s): \n");
-            int number = 1;
-            for (AdvertisementDTO advertisementDTO : advertisementsList) {
-                text.append(number++).append(". ").append(url).append(advertisementDTO.getId()).append(" will be closed ")
-                        .append(advertisementDTO.getEndTime().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"))).append("\n \n")
-                        .append(SIGNATURE);
+                StringBuilder text = new StringBuilder();
+                text.append("Dear ").append(contactInfo.getName()).append(",\n").append("your advertisement(s): \n");
+                int number = 1;
+                for (AdvertisementDTO advertisementDTO : advertisementsList) {
+                    text.append(number++).append(". ").append(url).append(advertisementDTO.getId()).append(" will be closed ")
+                            .append(advertisementDTO.getEndTime().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"))).append("\n \n")
+                            .append(SIGNATURE);
+                }
+                mailMessage.setText(text.toString());
+                emailSender.send(mailMessage);
+                log.info("Email is sent to {} at {}", email, LocalDateTime.now());
             }
-            mailMessage.setText(text.toString());
-            emailSender.send(mailMessage);
-            log.info("Email is sent to {}", email);
-        }
-        ));
+            ));
+        });
     }
 
     @Override
     public void sendEmailAboutExpired(Map<String, List<AdvertisementDTO>> emailAndAds) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        executor.execute(() -> {
+            log.info("Sending email about expired is started at {}", LocalDateTime.now());
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
 
-        emailAndAds.forEach(((email, advertisementsList) -> {
-            ContactInfoDTO contactInfo = advertisementsList.get(0).getOwner();
-            mailMessage.setTo(contactInfo.getEmail());
-            mailMessage.setSubject("Advertisement is closed");
-            StringBuilder text = new StringBuilder();
-            text.append("Dear ").append(contactInfo.getName()).append(",\n")
-                    .append("your advertisement(s): \n");
-            int number = 1;
-            for (AdvertisementDTO advertisementDTO : advertisementsList) {
-                text.append(number++).append(". ").append(url).append(advertisementDTO.getId()).append(" is closed ").append("\n \n")
-                        .append(SIGNATURE);
-            }
-            mailMessage.setText(text.toString());
-            emailSender.send(mailMessage);
-            log.info("Email is sent to {}", email);
-        }));
+            emailAndAds.forEach(((email, advertisementsList) -> {
+                ContactInfoDTO contactInfo = advertisementsList.get(0).getOwner();
+                mailMessage.setTo(contactInfo.getEmail());
+                mailMessage.setSubject("Advertisement is closed");
+                StringBuilder text = new StringBuilder();
+                text.append("Dear ").append(contactInfo.getName()).append(",\n")
+                        .append("your advertisement(s): \n");
+                int number = 1;
+                for (AdvertisementDTO advertisementDTO : advertisementsList) {
+                    text.append(number++).append(". ").append(url).append(advertisementDTO.getId()).append(" is closed ").append("\n \n")
+                            .append(SIGNATURE);
+                }
+                mailMessage.setText(text.toString());
+                emailSender.send(mailMessage);
+                log.info("Email is sent to {} at {}", email, LocalDateTime.now());
+            }));
+        });
     }
 
     @Override
     public void sendEmailAboutPurchase(AdvertisementDTO advertisement, ContactInfoDTO customer, AdOrderDTO adOrder) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        Set<ServiceDTO> servicesSet = adOrder.getServicesSet();
-        String services = servicesSet.stream()
-                .map(serviceDTO -> serviceDTO.getName() + ": " + serviceDTO.getPrice())
-                .collect(Collectors.joining(", "));
+        executor.execute(() -> {
+                log.info("Sending email about purchase is started at {}", LocalDateTime.now());
+                SimpleMailMessage mailMessage = new SimpleMailMessage();
+                Set<ServiceDTO> servicesSet = adOrder.getServicesSet();
 
-        mailMessage.setTo(customer.getEmail());
-        mailMessage.setSubject("You have done a purchase on \"CEREBRO\"");
-        StringBuilder text = new StringBuilder();
+                String services = servicesSet.stream()
+                        .map(serviceDTO -> serviceDTO.getName() + ": " + serviceDTO.getPrice())
+                        .collect(Collectors.joining(", "));
 
-        text.append("Dear ").append(customer.getName()).append(",\n")
-                .append("You have done a purchase ").append(url).append(advertisement.getId()).append("\n")
-                .append("Total price: ").append(adOrder.getTotalPrice());
-        if (!servicesSet.isEmpty()) {
-            text.append(" (additional services: ").append(services).append(")").append("\n");
+                mailMessage.setTo(customer.getEmail());
+                mailMessage.setSubject("You have done a purchase on \"CEREBRO\"");
+                StringBuilder text = new StringBuilder();
+
+                text.append("Dear ").append(customer.getName()).append(",\n")
+                        .append("You have done a purchase ").append(url).append(advertisement.getId()).append("\n")
+                        .append("Total price: ").append(adOrder.getTotalPrice());
+                if (!servicesSet.isEmpty()) {
+                    text.append(" (additional services: ").append(services).append(")").append("\n");
+                }
+                text.append("\n \n").append("If you haven't made this purchase, call or email us").append("\n \n")
+                        .append(SIGNATURE);
+
+                mailMessage.setText(text.toString());
+                emailSender.send(mailMessage);
+                log.info("Email is sent to {} at {}", customer.getEmail(), LocalDateTime.now());
+            });
         }
-        text.append("\n \n").append("If you haven't made this purchase, call or email us").append("\n \n")
-                .append(SIGNATURE);
-
-        mailMessage.setText(text.toString());
-        emailSender.send(mailMessage);
-        log.info("Email is sent to {}", customer.getEmail());
-    }
 
     @Override
     public void sendEmailAboutSell(AdvertisementDTO advertisement, ContactInfoDTO customer, AdOrderDTO adOrder) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        ContactInfoDTO seller = advertisement.getOwner();
+        executor.execute(() -> {
+            log.info("Sending email about sell is started at {}", LocalDateTime.now());
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            ContactInfoDTO seller = advertisement.getOwner();
+            Set<ServiceDTO> servicesSet = adOrder.getServicesSet();
 
-        Set<ServiceDTO> servicesSet = adOrder.getServicesSet();
-        String services = servicesSet.stream()
-                .map(serviceDTO -> serviceDTO.getName() + ": " + serviceDTO.getPrice())
-                .collect(Collectors.joining(", "));
+            String services = servicesSet.stream()
+                    .map(serviceDTO -> serviceDTO.getName() + ": " + serviceDTO.getPrice())
+                    .collect(Collectors.joining(", "));
 
-        mailMessage.setTo(seller.getEmail());
-        mailMessage.setSubject("You advertisement is closed on \"CEREBRO\"");
-        StringBuilder text = new StringBuilder();
+            mailMessage.setTo(seller.getEmail());
+            mailMessage.setSubject("You advertisement is closed on \"CEREBRO\"");
+            StringBuilder text = new StringBuilder();
 
-        text.append("Dear ").append(seller.getName()).append(",\n")
-                .append("Your advertisement ").append(url).append(advertisement.getId()).append(" is closed,").append("\n")
-                .append("because ").append(customer.getName()).append(" has made the purchase. \n")
-                .append("Total price: ").append(adOrder.getTotalPrice()).append("\n");
-        if(!servicesSet.isEmpty()){
-            text.append("Additional services were ordered: ").append(services).append("\n");
-        }
-        text.append("Contact information: ").append("tel: ").append(customer.getPhone())
-                .append(", email: ").append(customer.getEmail()).append("\n \n")
-                .append(SIGNATURE);
+            text.append("Dear ").append(seller.getName()).append(",\n")
+                    .append("Your advertisement ").append(url).append(advertisement.getId()).append(" is closed,").append("\n")
+                    .append("because ").append(customer.getName()).append(" has made the purchase. \n")
+                    .append("Total price: ").append(adOrder.getTotalPrice()).append("\n");
+            if (!servicesSet.isEmpty()) {
+                text.append("Additional services were ordered: ").append(services).append("\n");
+            }
+            text.append("Contact information: ").append("tel: ").append(customer.getPhone())
+                    .append(", email: ").append(customer.getEmail()).append("\n \n")
+                    .append(SIGNATURE);
 
-        mailMessage.setText(text.toString());
-        emailSender.send(mailMessage);
-        log.info("Email is sent to {}", customer.getEmail());
+            mailMessage.setText(text.toString());
+            emailSender.send(mailMessage);
+            log.info("Email is sent to {} at {}", customer.getEmail(), LocalDateTime.now());
+        });
     }
 }
---
+++ src/main/java/com/dataart/cerebro/exception/DataProcessingException.java
@@ -0,0 +1,4 @@
+package com.dataart.cerebro.exception;
+
+public class DataProcessingException {
+}
