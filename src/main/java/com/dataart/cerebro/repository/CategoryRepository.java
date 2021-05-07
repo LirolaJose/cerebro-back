4dc48bfe6dbb5bb0bebbcc33eff86597f6a223b4
klirola <khose.lirola@dataart.com>
2021-05-07 13:16:09 +0300

    fixed pom file

--- src/main/java/com/dataart/cerebro/repository/CategoryRepository.java
+++ src/main/java/com/dataart/cerebro/repository/CategoryRepository.java
@@ -1,14 +1,12 @@
 package com.dataart.cerebro.repository;
 
-import com.dataart.cerebro.domain.AdditionalService;
 import com.dataart.cerebro.domain.Category;
-import com.dataart.cerebro.domain.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Set;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     @Query(value = "SELECT * FROM category c join category_of_types cof on c.id = cof.category_id\n" +
             "join type t on t.id = cof.type_id WHERE t.id = :typeId ;", nativeQuery = true)
